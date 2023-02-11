7595338dd6ca80fb872fb24044f47beb58c537a3
Alejandro Minor <Alejandro@ROG-STRIX>
2023-02-10 23:57:16 -0600

    [FIX] Error en look and feel de windows

--- src/instalador2021/main.java
+++ src/instalador2021/main.java
@@ -33,12 +33,18 @@ public class main {
         
         try {
             
-            UIManager.setLookAndFeel(new WindowsLookAndFeel());
+            try {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
         } catch (UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
-            
-        }
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);}
         
    
         
